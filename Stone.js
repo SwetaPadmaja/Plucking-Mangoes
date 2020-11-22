c339f588dede9c2a235b7635e9499383ed75e114
Sweta <71880635+SwetaPadmaja@users.noreply.github.com>
2020-11-22 17:08:52 +0530

    Update Stone.js

--- Stone.js
+++ Stone.js
@@ -11,7 +11,7 @@ class Stone
 		this.x=x;
 		this.y=y;
 		this.r=r
-		this.image=loadImage("Plucking Mangoes/stone.png");
+		this.image=loadImage("stone.png");
 		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
 		World.add(world, this.body);
 
